5ed93128020ad7fb6972820a4700c427cc9a89e4
agb1a <agb1ad33@gmail.com>
2021-02-22 12:58:32 +0200

    Nychegosebe

---
+++ src/main/java/com/company/view/GetBy.java
@@ -0,0 +1,66 @@
+package com.company.view;
+
+import com.company.persons.Person;
+import com.company.persons.PersonsData;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GetBy {
+
+    public static Person getById(Integer id,PersonsData personStorage) {
+        List<Person> personList = personStorage.getPersons();
+        for (Person person : personList) {
+            if (person.getId().equals(id)) {
+                return person;
+            }
+        }
+        return null;
+    }
+    public static List<Person> getAllByCity(String city, PersonsData personStorage) {
+        List<Person> personList = personStorage.getPersons();
+        List<Person> resultList = new ArrayList<>();
+        for (Person person : personList) {
+            if (person.getCity().equals(city)) {
+                resultList.add(person);
+            }
+        }
+        return resultList;
+    }
+    public static List<Person> getAllByAge(Integer age, PersonsData personStorage) {
+        List<Person> personList = personStorage.getPersons();
+        List<Person> resultList = new ArrayList<>();
+        for (Person person : personList) {
+            if (person.getAge().equals(age)) {
+                resultList.add(person);
+            }
+        }
+        return resultList;
+    }
+    public static List<Person> getAllByFirstName(String firstName, PersonsData personStorage) {
+        List<Person> personList = personStorage.getPersons();
+        List<Person> resultList = new ArrayList<>();
+        for (Person person : personList) {
+            if (person.getFirstName().equals(firstName)) {
+                resultList.add(person);
+            }
+        }
+        return resultList;
+    }
+    public static List<Person> getAllByLastName(String lastName, PersonsData personStorage) {
+        List<Person> personList = personStorage.getPersons();
+        List<Person> resultList = new ArrayList<>();
+        for (Person person : personList) {
+            if (person.getLastName().equals(lastName)) {
+                resultList.add(person);
+            }
+        }
+        if (resultList.isEmpty()) {
+            System.out.println("No persons found");
+        }
+        return resultList;
+    }
+    public static void deleteAll(PersonsData personStorage ) {
+        List<Person> personList = personStorage.getPersons();
+        personList.clear();
+    }
+}
